00121a9459d5e7c4705f4f8b379627b21b5cc1ba
rramsay1 <126613261+rramsay1@users.noreply.github.com>
2023-03-12 23:30:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var animation = bodymovin.loadAnimation({
+
+container: document.getElementById('animation-container'),
+
+path: 'https://lottie.host/100f2e03-5f47-4eca-8928-08f610ba1c8f/Sch6kh76iM.json',
+
+renderer: 'svg',
+
+loop: true,
+
+autoplay: true,
+
+name: "Demo Animation",
+
+});
